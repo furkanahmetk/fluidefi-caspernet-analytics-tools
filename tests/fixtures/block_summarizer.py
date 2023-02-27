11f5de7e1e921e15aed2906a62a99b2911f82f90
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-02-27 13:15:27 +0400

    finalized unit test

---
+++ tests/fixtures/block_summarizer.py
@@ -0,0 +1,44 @@
+blocks = [
+    {"block_number": 1393081, "timestamp_utc": '2023-01-09 09:55:39.648+00'},
+    {"block_number": 1393082, "timestamp_utc": '2023-01-09 09:57:17.952+00'},
+    {"block_number": 1393083, "timestamp_utc": '2023-01-09 10:45:54.304+00'}
+]
+
+pairs = [
+    {"contract_address": 'cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0'},
+    {"contract_address": 'dedca9e5412d973a8cecaf9d98ce2a60d0d4f62be0b22a9eb3e19247f67c697b'},
+    {"contract_address": '800dee0fb5abf6d3525f520a4b052d8d36edb985a748a671209745c80836c2af'},
+    {"contract_address": 'a3f3a7c26a0723f56ad74dcb4d9a86642d1d53c6d1add00c237df5199a3025e6'},
+    {"contract_address": 'e6b4a934630aee279665c99dc2bf4219872b7178990cd7fee7634a0f1362b591'},
+    {"contract_address": '95f1253782197e05e3d260e713a5828ada1ec98856e3908536263890d27163f0'}
+]
+
+sync_events = [
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "reserve0": 1000000000, "reserve1": 27800000},
+    {"block_number": 1393448, "address": "a84382872d1402a5ec8d8453f516586166100d8252f997b0bcdeed8c4737588d", "reserve0": 10677395101998, "reserve1": 919041928425877},
+    {"block_number": 1393532, "address": "cb0f9f291ae73928b739c90c03eca70cd610d945304ea606fe4adced3fa07060", "reserve0": 144100081137, "reserve1": 54042057076274},
+]
+
+mint_events = [
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0": 1000000000, "amount1": 27800000},
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0": 10677395101998, "amount1": 919041928425877},
+    {"block_number": 1393532, "address": "cb0f9f291ae73928b739c90c03eca70cd610d945304ea606fe4adced3fa07060", "amount0": 144100081137, "amount1": 54042057076274},
+]
+
+burn_events = [
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0": 1000000000, "amount1": 27800000},
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0": 10677395101998, "amount1": 919041928425877},
+    {"block_number": 1393532, "address": "cb0f9f291ae73928b739c90c03eca70cd610d945304ea606fe4adced3fa07060", "amount0": 144100081137, "amount1": 54042057076274},
+]
+
+swap_events = [
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0_in": 1000000000, "amount0_out": 27800000, "amount1_in": 0, "amount1_out": 0},
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "amount0_in": 0, "amount0_out": 0, "amount1_in": 1000000000, "amount1_out": 27800000},
+    {"block_number": 1393532, "address": "cb0f9f291ae73928b739c90c03eca70cd610d945304ea606fe4adced3fa07060", "amount0_in": 1000000000, "amount0_out": 27800000, "amount1_in": 0, "amount1_out": 0},
+]
+
+total_supply = [
+    {"block_number": 1393081, "address": "cf56e334481fe2bf0530e0c03a586d2672da8bfe1d1d259ea91457a3bd8971e0", "total_supply": 19421699626},
+    {"block_number": 1393091, "address": "800dee0fb5abf6d3525f520a4b052d8d36edb985a748a671209745c80836c2af", "total_supply": 166755238},
+    {"block_number": 1393532, "address": "cb0f9f291ae73928b739c90c03eca70cd610d945304ea606fe4adced3fa07060", "total_supply": 280542344},
+]
