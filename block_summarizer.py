646db85658ff95142ff9b30ba79ec2154b19ecdf
MessaoudAzidane <fm_azidane@esi.dz>
2023-02-27 14:44:29 +0100

    docker-compose working fine

--- block_summarizer.py
+++ block_summarizer.py
@@ -8,6 +8,8 @@ import django
 django.setup()
 from cspr_summarization.entities.BlockSummary import BlockSummary
 from cspr_summarization.entities.BlockHours import BlockHours
+import schedule
+import time
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
@@ -15,8 +17,12 @@ from cspr_summarization.entities.BlockHours import BlockHours
 from cspr_summarization.services.lp_block_summarizer.lp_block_summarizer import LPBlockSummarizer
 
 def main():
-    latest_block_summary = BlockSummary.objects.using('default').values('block_number').latest('block_number')['block_number']
     latest_block_hour = BlockHours.objects.using('default').values('block_number').latest('block_number')['block_number']
+    try:
+        latest_block_summary = BlockSummary.objects.using('default').values('block_number').latest('block_number')['block_number']
+    except:
+        latest_block_summary = BlockHours.objects.using('default').values('block_number').order_by('block_number').first()['block_number']
+
     
     if latest_block_hour >= latest_block_summary:
         for i in range(latest_block_summary, latest_block_hour + 1):
@@ -24,5 +30,10 @@ def main():
             lpBlockSummarizer.summarizer()
 
 if __name__ == '__main__':
-    main()
-    
+    schedule.every(1).minutes.do(main)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        main()
+        
--- hourly_summarizer.py
+++ hourly_summarizer.py
@@ -10,65 +10,74 @@ django.setup()
 from cspr_summarization.entities.HourlyData import HourlyData
 from cspr_summarization.entities.BlockHours import BlockHours
 import pandas as pd
-from cspr_summarization.services.lp_hourly_summarizer import LpHourlySummarizer
+from cspr_summarization.services.lp_hourly_summarizer.lp_hourly_summarizer import LpHourlySummarizer
 import pytz
 from datetime import datetime, timedelta
 import logging
+import schedule
+import time
 
 logging.basicConfig(level=logging.INFO)
 
-last_hourly_block_timestamp = BlockHours.objects.values('hourly_timestamp_utc').order_by('-hourly_timestamp_utc').first()['hourly_timestamp_utc']
+def run_hourly_summarizer():
+  last_hourly_block_timestamp = BlockHours.objects.values('hourly_timestamp_utc').order_by('-hourly_timestamp_utc').first()['hourly_timestamp_utc']
 
-# try to get the last houly data timestamp
-try:
-  last_end_hour = HourlyData.objects.values('close_timestamp_utc').order_by('-close_timestamp_utc').first()['close_timestamp_utc']
-# in case the table is empty!
-# then get the min hourly data timestamp (from BlockHours)
-except:
-  last_end_hour = BlockHours.objects.values('hourly_timestamp_utc').order_by('hourly_timestamp_utc').first()['hourly_timestamp_utc']
+  # try to get the last houly data timestamp
+  try:
+    last_end_hour = HourlyData.objects.values('close_timestamp_utc').order_by('-close_timestamp_utc').first()['close_timestamp_utc']
+  # in case the table is empty!
+  # then get the min hourly data timestamp (from BlockHours)
+  except:
+    last_end_hour = BlockHours.objects.values('hourly_timestamp_utc').order_by('hourly_timestamp_utc').first()['hourly_timestamp_utc']
 
-# Set microseconds, seconds, & minutes to 0
-last_end_hour = last_end_hour.replace(microsecond=0, second=0, minute=0)
-last_hourly_block_timestamp = last_hourly_block_timestamp.replace(microsecond=0, second=0, minute=0)
+  # Set microseconds, seconds, & minutes to 0
+  last_end_hour = last_end_hour.replace(microsecond=0, second=0, minute=0)
+  last_hourly_block_timestamp = last_hourly_block_timestamp.replace(microsecond=0, second=0, minute=0)
 
-# Get the start and end hour
-next_start_hour = last_end_hour
-next_end_hour = (next_start_hour + timedelta(hours=1))
-
-# Already summarized data for this hour
-if next_start_hour > last_hourly_block_timestamp:
-  print(f'All good for: {next_start_hour} - {next_end_hour}')
-  exit(0)
-
-# While not done with all the missed past hours Do ...
-while last_hourly_block_timestamp >= next_start_hour:
-
-  summarizer = LpHourlySummarizer(next_start_hour, next_end_hour)
-  # Initilize Data (all column at 0)
-  summarizer.init_hourly_data()
-  logging.info(f'Summarization started {next_start_hour} - {next_end_hour}...')
-
-  # At least one block on the last hour
-  if len(summarizer.last_hour_block_numbers) > 0 :
-    logging.info(f'\t Sync Summarizing data...')
-    summarizer.sync_consumer()
-    logging.info(f'\t Mint Summarizing data...')
-    summarizer.mint_consumer()
-    logging.info(f'\t Burn Summarizing data...')
-    summarizer.burn_consumer()
-    logging.info(f'\t Swap Summarizing data...')
-    summarizer.swap_consumer()
-    logging.info(f'\t Clost lp token Summarizing data...')
-    summarizer.close_lp_token_supply_consumer()
-    logging.info(f'\t Max Block Summarizing data...')
-    summarizer.max_block_consumer()
-    logging.info(f'\t End')
-  # No block on the last hour
-  else:
-    logging.info(f'No blocks have been created between {next_start_hour} - {next_end_hour}')
-
-  #
-  next_start_hour = (next_start_hour + timedelta(hours=1))
+  # Get the start and end hour
+  next_start_hour = last_end_hour
   next_end_hour = (next_start_hour + timedelta(hours=1))
-  
 
+  # Already summarized data for this hour
+  if next_start_hour > last_hourly_block_timestamp:
+    logging.info(f'All good for: {next_start_hour} - {next_end_hour}')
+
+  # While not done with all the missed past hours Do ...
+  while last_hourly_block_timestamp >= next_start_hour:
+
+    summarizer = LpHourlySummarizer(next_start_hour, next_end_hour)
+    # Initilize Data (all column at 0)
+    summarizer.init_hourly_data()
+    logging.info(f'Summarization started {next_start_hour} - {next_end_hour}...')
+
+    # At least one block on the last hour
+    if len(summarizer.last_hour_block_numbers) > 0 :
+      logging.info(f'\t Sync Summarizing data...')
+      summarizer.sync_consumer()
+      logging.info(f'\t Mint Summarizing data...')
+      summarizer.mint_consumer()
+      logging.info(f'\t Burn Summarizing data...')
+      summarizer.burn_consumer()
+      logging.info(f'\t Swap Summarizing data...')
+      summarizer.swap_consumer()
+      logging.info(f'\t Clost lp token Summarizing data...')
+      summarizer.close_lp_token_supply_consumer()
+      logging.info(f'\t Max Block Summarizing data...')
+      summarizer.max_block_consumer()
+      logging.info(f'\t End')
+    # No block on the last hour
+    else:
+      logging.info(f'No blocks have been created between {next_start_hour} - {next_end_hour}')
+
+    #
+    next_start_hour = (next_start_hour + timedelta(hours=1))
+    next_end_hour = (next_start_hour + timedelta(hours=1))
+    
+
+
+
+schedule.every(1).minute.do(run_hourly_summarizer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
