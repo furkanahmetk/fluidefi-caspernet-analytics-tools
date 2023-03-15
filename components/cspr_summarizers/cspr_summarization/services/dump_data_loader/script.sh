48db792b84d17da55ef231822452f78b89e18d2d
MessaoudAzidane <fm_azidane@esi.dz>
2023-03-15 14:27:19 +0100

    refactoring env vars

--- components/cspr_summarizers/cspr_summarization/services/dump_data_loader/script.sh
+++ components/cspr_summarizers/cspr_summarization/services/dump_data_loader/script.sh
@@ -1,18 +1,11 @@
 #
 set -e
 
-DB_HOST=$1
-DB_PORT=$2
-DB_NAME=$3
-DB_USER=$4
-DB_PASSWORD=$5
-IMPORT_DATA=$6
-
 
 if [ "$IMPORT_DATA" = true ]; then
   echo "Starting import..."
 
-  psql -h $DB_HOST -U $DB_USER -d $DB_NAME  -f ./dump_data
+  psql -h $WRITE_DB_CONNECTION_HOST -U $WRITE_DB_CONNECTION_USERNAME -d $WRITE_DB_CONNECTION_DATABASE   -f ./dump_data
 
   echo "Import complete."
 else
