d8251dfc5dd2b87f618e532da423c73ffc547b9b
MessaoudAzidane <fm_azidane@esi.dz>
2023-03-16 10:40:08 +0100

    removed unnecessary env var

--- components/cspr_summarizers/cspr_summarization/services/dump_data_loader/script.sh
+++ components/cspr_summarizers/cspr_summarization/services/dump_data_loader/script.sh
@@ -2,14 +2,12 @@
 set -e
 
 
-if [ "$IMPORT_DATA" = true ]; then
-  echo "Starting import..."
 
-  psql -h $WRITE_DB_CONNECTION_HOST -U $WRITE_DB_CONNECTION_USERNAME -d $WRITE_DB_CONNECTION_DATABASE -f ./dump_data.sql
+echo "Starting import..."
 
-  echo "Import complete."
-else
-  echo "Dump Data not being imported. The value of the env var 'IMPORT_DATA' is not set to true."
-fi
+psql -h $WRITE_DB_CONNECTION_HOST -U $WRITE_DB_CONNECTION_USERNAME -d $WRITE_DB_CONNECTION_DATABASE -f ./dump_data.sql
+
+echo "Import complete."
+
 
 
